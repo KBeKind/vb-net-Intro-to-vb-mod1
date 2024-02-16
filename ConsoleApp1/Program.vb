33e29ac2e85817a7985f271b45dc0385596b3419
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-02-16 12:33:22 -0600

    commit

--- ConsoleApp1/Program.vb
+++ ConsoleApp1/Program.vb
@@ -4,7 +4,89 @@ Module Program
 
     Sub Main(args As String())
 
-        Console.WriteLine("Welcome to Coursera " & Console.ReadLine())
+        'Console.WriteLine("Welcome to Coursera " & Console.ReadLine())
+
+
+        'Dim salary As String = Console.ReadLine()
+
+        'salary = FormatNumber(salary, 2)
+
+        'Console.WriteLine(salary)
+
+
+        'Dim numberToDivide As String = Console.ReadLine()
+
+        'Dim numberToDivideInt = CInt(numberToDivide)
+
+        'Dim workingValue As Decimal = (numberToDivideInt / 3)
+
+        'Dim formattedResult As Decimal = Math.Round(workingValue, 3)
+
+        'Console.WriteLine(FormatNumber(formattedResult, 3))
+
+
+        'Dim yourname As String
+
+        'yourname = Console.ReadLine()
+
+        'Console.Write("Hello ")
+
+        'If (yourname = "Sally") Then
+        '    Console.WriteLine("Sally my friend")
+        'ElseIf (yourname = "Fred") Then
+        '    Console.WriteLine("Fred my exfriend")
+        'Else
+        '    Console.WriteLine(yourname)
+        'End If
+
+
+
+        'Select Case yourname
+
+        '    Case "Sally"
+        '        Console.WriteLine("Sally my friend")
+
+        '    Case "Fred"
+        '        Console.WriteLine("Fred my exfriend")
+
+        '    Case Else
+        '        Console.WriteLine(yourname)
+
+        'End Select
+
+
+
+        'Dim userNumberString As String
+
+        'userNumberString = Console.ReadLine()
+
+        'Dim userNumberInteger As Integer = Integer.Parse(userNumberString)
+
+        'If (userNumberInteger < 100) Then
+        '    Console.WriteLine("Small")
+        'ElseIf (userNumberInteger < 200) Then
+        '    Console.WriteLine("Medium")
+        'Else
+        '    Console.WriteLine("Large")
+        'End If
+
+        'Select Case userNumberInteger
+        '    Case Is < 100
+        '        Console.WriteLine("Small")
+
+        '    Case Is < 200
+        '        Console.WriteLine("Medium")
+
+        '    Case Else
+        '        Console.WriteLine("Large")
+        'End Select
+
+
+
+
+
+
+
 
     End Sub
 
