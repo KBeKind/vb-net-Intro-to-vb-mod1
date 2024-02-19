949f9453ea1e5e3b78c39e0feba6c3d8873baa44
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-02-18 18:56:45 -0600

    commit

--- VBIntroModule4/Cow.vb
+++ VBIntroModule4/Cow.vb
@@ -1,32 +1,80 @@
-﻿Public Class Cow : Inherits Animal
+﻿Public Class Cow : Implements Animal, IComparable
 
+    Private _height As Double
+    Private _weight As Double
 
-    Public spots As Integer
+    Public Property Height As Double Implements Animal.Height
+        Get
+            Return _height
+        End Get
+        Set(value As Double)
+            _height = value
+        End Set
+    End Property
 
-    Public Overrides Sub Walk(inDistance As Integer)
-        Console.WriteLine(String.Format("Cow walks {0}", inDistance))
-    End Sub
+    Public Property Weight As Double Implements Animal.Weight
+        Get
+            Return _weight
+        End Get
+        Set(value As Double)
+            _weight = value
+        End Set
+    End Property
 
+    Public Function Speed() As Double Implements Animal.Speed
+        Return 11
+    End Function
 
+    Public Function CompareTo(obj As Object) As Integer Implements IComparable.CompareTo
 
-    Public Overrides Function Equals(ByVal inObj As Object) As Boolean
+        If TypeOf obj Is Cow Then
 
-        If TypeOf inObj Is Cow Then
+            If CType(obj, Cow).Height = Height And CType(obj, Cow).Weight = Weight And CType(obj, Cow).Speed() = Speed() Then
 
-            Return CType(inObj, Cow).spots = spots
+                Return 0
 
-        Else
+            ElseIf CType(obj, Cow).Speed() >= Speed() Then
 
-            Return inObj.GetHashCode = MyClass.GetHashCode
+                Return -1
+
+            Else
+                Return 1
+
+            End If
+
+            Return -1
 
         End If
 
     End Function
 
 
-    Public Overloads Function Equals(ByVal inCow As Cow) As Boolean
-        Return inCow.spots = spots
-    End Function
+    'Public spots As Integer
+
+    'Public Overrides Sub Walk(inDistance As Integer)
+    '    Console.WriteLine(String.Format("Cow walks {0}", inDistance))
+    'End Sub
+
+
+
+    'Public Overrides Function Equals(ByVal inObj As Object) As Boolean
+
+    '    If TypeOf inObj Is Cow Then
+
+    '        Return CType(inObj, Cow).spots = spots
+
+    '    Else
+
+    '        Return inObj.GetHashCode = MyClass.GetHashCode
+
+    '    End If
+
+    'End Function
+
+
+    'Public Overloads Function Equals(ByVal inCow As Cow) As Boolean
+    '    Return inCow.spots = spots
+    'End Function
 
 
 
